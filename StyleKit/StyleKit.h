7f10e0c19973488a2987bc86525f47bcc5a9cd56
Jakub Petrík <petrik@inloop.eu>
2017-11-13 09:49:13 +0100

    Remove DirectoryWatcher

--- StyleKit/StyleKit.h
+++ StyleKit/StyleKit.h
@@ -9,4 +9,3 @@ FOUNDATION_EXPORT double StyleKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char StyleKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <StyleKit/PublicHeader.h>
-#import <StyleKit/DirectoryWatcher.h>
